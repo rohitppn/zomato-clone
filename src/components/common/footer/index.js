adec679cd81ec12efb1bfdc1ba245898b598a4b0
Rohit Sharma <mail2rohitsharmappn@gmail.com>
2023-07-04 18:31:15 +0530

    zomato clone website

---
+++ src/components/common/footer/index.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div>
+      Made By 🔥Rohit Sharma🔥
+    </div>
+  )
+}
+
+export default Footer
---
+++ src/components/common/header/index.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import './header.css';
+
+const Header = () => {
+  return <div className="max-width header">
+    <img 
+    src='https://b.zmtcdn.com/web_assets/b40b97e677bc7b2ca77c58c61db266fe1603954218.png' alt='Zomata-log' className='header-logo' />
+    
+    <div className="header-right">
+      <div className="header-location-search-container">
+        <div className="location-wrapper">
+          <div className="location-icon-name">
+          <i className="fi fi-rr-marker absolute-center location-icon"></i>
+          <div>Banglore</div>
+          </div>
+          <i className="fi fi-rr-caret-down absolute-center"></i>
+        </div>
+        <div className="location-search-seperator"></div>
+        <div className="header-searchbar">
+          <i className="fi fi-rr-search absolute-center search-icon"></i>
+          <input placeholder='search for restaurant, cuisine or a dish' className='search-input' />
+        </div>
+      </div>
+      <div className='profile-wrapper'>
+        <img src='https://b.zmtcdn.com/data/user_profile_pictures/7b9/ec98e2b7b5f41596705f5773450b97b9.jpg?fit=around%7C100%3A100&crop=100%3A100%3B%2A%2C%2A' alt='profile' className='header-profile-image' />
+        <span className='header-username'>Rohit</span>
+        <i className='fi fi-rr-angle-small-down absolute-center profile-options-icon'></i>
+      </div>
+    </div>
+    </div>
+}
+
+export default Header
